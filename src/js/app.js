5f7e8ecd7b08ec8c30b28369b1704dd512055a9d
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-09-23 16:39:16 +1000

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import 'normalize.css/normalize.css';
+import '../scss/styles.scss';
+import * as d3 from "d3"
+import { importImages } from './importImages'
+
+document.querySelector('h1').innerHTML = 'Dynamic Heading'
+console.log('Test console.log')
+
+// console.log(d3)
